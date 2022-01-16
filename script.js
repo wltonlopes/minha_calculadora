40d9bbe765fae406a0622c2fbd4fce41d8e873c9
Welton Lopes <weltonlopesmetal@gmail.com>
2022-01-16 17:00:54 -0300

    anotaçoes inicio

--- script.js
+++ script.js
@@ -2,3 +2,29 @@ function displaynum(n1){
   calculadora.display.value = calculadora.display.value + n1;
 
 }
+
+const salvar = (a) =>{
+  const anotacao = a;
+  const anotacao_salva = localStorage.getItem('anotacao');
+  if(anotacao_salva == null){
+    localStorage.setItem('anotacao',anotacao);
+  }
+  else{
+    localStorage.setItem('anotacao',anotacao_salva + ' ./new ' + anotacao);
+  }
+  lista_salva();
+}
+
+const lista_salva = () =>{
+  const anotacoes_salvas = localStorage.getItem('anotacao');
+  const lista = document.getElementById('lista_anotacoes_salvas');
+  const listas_separadas = anotacoes_salvas.split(' ./new ');
+  lista.innerHTML = '';
+  for (let value of listas_separadas){
+    const li = document.createElement('li');
+    // li.innerText = value;
+    li.appendChild(document.createTextNode(value));
+    lista.appendChild(li);
+  }
+  // lista.innerHTML = anotacoes_salvas;
+}
